6a6c2b13504fbf06e12c40242ba367031aee2151
saswatuia <74714623+saswatuia@users.noreply.github.com>
2020-11-29 19:39:46 +0100

    Update todoliste.js

--- public/todoliste.js
+++ public/todoliste.js
@@ -1,6 +1,6 @@
 let todoInput = document.querySelector(".todo-input");
 let todoButton = document.querySelector(".todo-button");
-let todoList = document.querySelector(".todo-list");
+let todoList = document.querySelector(".todo-liste");
 
 
 //event listner
